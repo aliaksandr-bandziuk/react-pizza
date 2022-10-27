5bc265a9267df9bac4179ca379911728ca96c49b
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-10-27 16:23:08 +0300

    create useStates to pizzaBlock

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Project React-Pizza
 
-## Features
+## Stack
 
 In project I used next features:
 
+- ReactJS 18
+- TypeScript
+- Redux Toolkit (хранение данных / пицц)
+- React Router v6 (навигация)
+- Axios + Fetch (отправка запроса на бэкенд)
+- React Hooks (хуки)
+- CSS-Modules / SCSS (стилизация)
+- React Content Loader (скелетон)
+- React Pagination (пагинация)
+- Lodash.Debounce
+- Code Splitting, React Loadable, useWhyDidYouUpdate
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Categories = () => {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    'All',
+    'Meat',
+    'Vegetarian',
+    'Grill',
+    'Chili'
+  ]
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  }
+
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? "active" : ''}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   )
 }
 
-export default Categories
+export default Categories;
